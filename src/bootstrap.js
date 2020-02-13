5bde6aa092b4ae55be3864774c2247b278337de4
danzjamz <djl.hovley@gmail.com>
2020-02-12 22:19:37 -0700

    initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,8 @@
+// import classChallenge, { welcome, sum } from './classChallenge';
+import * as cc from './classChallenge';
+
+console.log(cc.default());
+
+console.log(cc.welcome);
+
+console.log(cc.sum(1, 2));
---
+++ src/classChallenge.js
@@ -0,0 +1,9 @@
+export default function() {
+    return '<div>classChallenge</div>'
+}
+
+export const welcome = 'welcome';
+
+export function sum(num1, num2) {
+    return num1 + num2;
+}
